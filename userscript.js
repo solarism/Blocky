ce97bc989127f3e15c57469040d4b307cd35a87b
makotofu <mercuryrpcl@gmail.com>
2016-08-10 10:16:57 +1000

    Added missing semicolons

--- userscript.js
+++ userscript.js
@@ -3,20 +3,20 @@
 // @namespace    https://github.com/qwgsr/Blocky
 // @version      0.1
 // @description  DET Website Blocking Userscript
-// @author       solarism
+// @author       makotofu
 // @match        http*://*/*
 // @grant        none
 // ==/UserScript==
 if(window.location.protocol.indexOf("https") > -1){
-    protocol = "https:"
+    protocol = "https:";
 } else {
-    protocol = "http:"
+    protocol = "http:";
 }
-var host = protocol+"//"+window.location.hostname
-var url = window.location.href
+var host = protocol+"//"+window.location.hostname;
+var url = window.location.href;
 if(url.indexOf("http://appserver.det.nsw.edu.au/bfp/action/blocksf?actionreason=by-category&") > -1){
     blocked = true;
 } else {
     document.title = "403 Denied";
-    window.open("http://appserver.det.nsw.edu.au/bfp/action/blocksf?actionreason=by-category&username=solarism&requestedurl="+host+"&categorydescriptionlist=none&pn=","_self");
+    window.open("http://appserver.det.nsw.edu.au/bfp/action/blocksf?actionreason=by-category&username=makotofu&requestedurl="+host+"&categorydescriptionlist=none&pn=","_self");
 }
